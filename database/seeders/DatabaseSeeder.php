1e9398b0aaf8e9bb12f31cb97249b64c5a8fe636
Berat <bharurluoglu@hotmail.com>
2022-10-21 21:57:38 +0300

    Add Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,12 +14,12 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    {   
+        DB::table('places')->truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            CountriesTableSeeder::class,
+            PlacesTableSeeder::class,
+        ])->command->info('Seeded the database! ');
     }
 }
